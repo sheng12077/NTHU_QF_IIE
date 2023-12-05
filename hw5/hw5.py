74bcc711c0af4e32b1aefb52070ce5139051cca3
sheng-qian ke <141042325+sheng12077@users.noreply.github.com>
2023-12-05 18:17:43 +0800

    Create hw5.py

---
+++ hw5/hw5.py
@@ -0,0 +1,11 @@
+def sentence(s):
+    ans=[]
+    for j in range(1,51):
+        for i in range(len(s)):
+            if len(s[i])==j and s[i] not in ans:
+                ans.append(s[i])
+    return ans
+def test():    
+    s=input().lower().replace('.',' ').replace(',',' ').split(' ')
+    print(sentence(s)) 
+test()
